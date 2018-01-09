34da4a940c12dadb9c3195723c75ec5fe5c91ba1
Shawn Iwinski <siwinski@redhat.com>
2018-01-09 17:27:18 -0500

    Add license header to tests' bootstrap file

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * This file is part of the RHDC Akamai middleware package.
+ *
+ * (c) Shawn Iwinski <siwinski@redhat.com>
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
 
 // Display all errors
 error_reporting(E_ALL);
