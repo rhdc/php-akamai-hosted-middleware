2510ed2343ad0c42e3fd07ca2aedd9c73a750d82
Shawn Iwinski <siwinski@redhat.com>
2018-01-10 14:01:32 -0500

    [AssertStagingMiddleware] Initial implementation

---
+++ src/Response/AssertStagingMiddlewareException.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * This file is part of the RHDC Akamai middleware package.
+ *
+ * (c) Shawn Iwinski <siwinski@redhat.com>
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+namespace Rhdc\Akamai\Middleware\Response;
+
+class AssertStagingMiddlewareException extends \Exception
+{
+}
